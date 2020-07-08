5b5578768a2a94c7db77ede4ec9fa0656bafbf1d
Akash MEHRA <akash.mehra99@gmail.com>
2020-07-08 17:45:29 +0530

    updated mock data

---
+++ src/mock-data/mock_data.js
@@ -0,0 +1,64 @@
+export const mockData = [
+    {   id: 1,
+        status: 'on-track',
+        description: 'Text On Track 1',
+        targetDate: '01/11/2020',
+        status: '70%'
+    },
+    {   
+        id: 2,
+        status: 'on-track',
+        description: 'Text On Track 2',
+        targetDate: '01/08/2020',
+        status: '40%'
+    },
+    {
+        id: 3,
+        status: 'on-track',
+        description: 'Text On Track 3',
+        targetDate: '01/09/2020',
+        status: '70%'
+    },
+    {
+        id: 4,
+        status: 'delayed',
+        description: 'Text Delayed 1',
+        targetDate: '01/11/2020',
+        status: '70%'
+    },
+    {
+        id: 5,
+        status: 'delayed',
+        description: 'Text Delayed 2',
+        targetDate: '01/08/2020',
+        status: '20%'
+    },
+    {
+        id: 6,
+        status: 'delayed',
+        description: 'Text Delayed 3',
+        targetDate: '01/06/2020',
+        status: '10%'
+    },
+    {
+        id: 7,
+        status: 'on-hold',
+        description: 'Text on Hold 1',
+        targetDate: '01/08/2020',
+        status: '20%'
+    },
+    {
+        id: 8,
+        status: 'on-hold',
+        description: 'Text on Hold 2',
+        targetDate: '01/09/2020',
+        status: '33%'
+    },
+    {
+        id: 9,
+        status: 'on-hold',
+        description: 'Text on Hold 3',
+        targetDate: '01/08/2020',
+        status: '53%'
+    }
+];
